e9da9fba3dda76febbbbac7a25fb4e6e4ade1b31
Pedro <pedro@gmail.com>
2023-09-11 09:03:41 -0300

    v1

---
+++ AppRpgEtec/Services/Usuarios/UsuarioService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+
+namespace AppRpgEtec.Services.Usuarios
+{
+    public class UsuarioService: Request
+    {
+        private readonly Request _request;
+        private const string apiUrlBase = "http://xyz.somee.com/RpgApi/Usuarios";
+        
+        public UsuarioService()
+        {
+            _request = new Request();
+        }
+
+        public async Task<Usuario> PostRegistrarUsuarioAsync(Usuario u)
+        {
+            string urlComplentar = "/Registrar";
+            u.Id = await _request.PostReturnIntAsync(apiUrlBase + urlComplentar, u);
+            return u;
+        }
+
+
+        public async Task<Usuario> PostAutenticarUsuarioAsync(Usuario u)
+        {
+            string urlComplementar = "/Autenticar";
+            u = await _request.PostAsync(apiUrlBase + urlComplementar, u, String.Empty); 
+            return u;
+        }
+    }
+
+       
+}
---
+++ AppRpgEtec/ViewModels/Usuario/UsuarioViewModel.cs
@@ -0,0 +1,59 @@
+﻿using AppRpgEtec.Services.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AppRpgEtec.Models;
+
+namespace AppRpgEtec.ViewModels.Usuario
+{
+    public class UsuarioViewModel : BaseViewModel
+    {
+        private UsuarioService uService;
+        public ICommand RegistrarCommand { get; set; }
+
+
+        private string login = string.Empty;
+        public string Login
+        {
+            get { return login; }
+            set
+            {
+                login = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string senha = string.Empty;
+        public string Senha
+        {
+            get { return senha; }
+            set
+            {
+                senha = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async Task RegistrarUsuario()
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Username = Login;
+                u.PasswordString  = senha;
+
+                Usuario uRegistrado.Id != 0
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+    }
+}
+
